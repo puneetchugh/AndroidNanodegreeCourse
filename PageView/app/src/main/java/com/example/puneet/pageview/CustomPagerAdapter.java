04dde2d22684bd78c7743112b16c342081121818
Puneet <puneet.chugh@nyu.edu>
2016-03-21 21:24:14 -0400

    PageView Demo app

---
+++ PageView/app/src/main/java/com/example/puneet/pageview/CustomPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.puneet.pageview;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by puneet on 3/14/16.
+ */
+class CustomPagerAdapter extends PagerAdapter {
+
+    int[] mResources = {
+            R.drawable.sachin,
+            R.drawable.dhoni,
+            R.drawable.dhawan,
+            R.drawable.villiers,
+            R.drawable.akhtar,
+            R.drawable.gayle
+    };
+
+    Context mContext;
+    LayoutInflater mLayoutInflater;
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return mResources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(mResources[position]);
+
+        container.addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
